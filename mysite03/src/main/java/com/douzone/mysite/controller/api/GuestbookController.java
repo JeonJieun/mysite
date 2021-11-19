bc1cfb503d6f3e5d6b48955ab815c2c9178624bc
JeonJieun <wjn135@naver.com>
2021-11-19 10:50:23 +0900

    mysite03

--- mysite03/src/main/java/com/douzone/mysite/controller/api/GuestbookController.java
+++ mysite03/src/main/java/com/douzone/mysite/controller/api/GuestbookController.java
@@ -1,60 +1,38 @@
 package com.douzone.mysite.controller.api;
 
+import com.douzone.mysite.dto.JsonResult;
+import com.douzone.mysite.vo.GuestbookVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.douzone.mysite.service.GuestbookService;
+
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.douzone.mysite.dto.JsonResult;
-import com.douzone.mysite.service.GuestbookService;
-import com.douzone.mysite.vo.GuestbookVo;
-
-@Controller("guestbookApiController")
-@RequestMapping("/api/guestbook")
+@RestController("guestbookControllerApi")
+@RequestMapping("/guestbook/api")
 public class GuestbookController {
-
 	@Autowired
-	GuestbookService guestbookService;
-	
-	@ResponseBody
-	@RequestMapping("/add")
-	public JsonResult ex1(@RequestBody GuestbookVo vo) {
-		guestbookService.addMessage(vo);
-		return JsonResult.success(vo);
-	}
-	
-	@ResponseBody
-	@RequestMapping("/list")
-	public JsonResult ex2(
-		@RequestParam(value="sn", required=true, defaultValue="-1") Long no) {
-		//List<GuestbookVo> list = new ArrayList<>();
-		
-		//GuestbookVo vo = guestbookService.findByNo(no);
-		//list.add(vo);
-		
-		List<GuestbookVo> list = guestbookService.getMessageList();
+	private GuestbookService guestbookService;
 
+	@GetMapping("/list/{no}")
+	public JsonResult list(@PathVariable("no") Long startNo) {
+		List<GuestbookVo> list = guestbookService.getMessageList(startNo);
 		return JsonResult.success(list);
 	}
-		
-	@ResponseBody
-	@RequestMapping("/delete/{no}")
-	public JsonResult ex3(@PathVariable("no") Long no, String password) {
-		
-		Long data = 0L;
-		
-		if(guestbookService.deleteMessage(no, password)) {
-			data = no;
-		}else {
-			data = -1L;
-		}
-		
-		return JsonResult.success(data);
+
+	@PostMapping("/add")
+	public JsonResult add(@RequestBody GuestbookVo vo) {
+		guestbookService.addMessage(vo);
+		vo.setPassword("");
+		return JsonResult.success(vo);
 	}
-	
+
+	@DeleteMapping("/delete/{no}")
+	public JsonResult delete(
+			@PathVariable("no") Long no,
+			@RequestParam(value="password", required=true, defaultValue="") String password) {
+		boolean result = guestbookService.deleteMessage(no, password);
+		return JsonResult.success(result ? no : -1);
+	}
 }
